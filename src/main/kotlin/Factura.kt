77debdd0332d82ea1fce2386cbd37b9ed6e7c9b9
ivanboot <ivanma.garcia@hotmail.com>
2024-03-02 23:07:25 -0600

    Completando proyecto

--- src/main/kotlin/Factura.kt
+++ src/main/kotlin/Factura.kt
@@ -7,40 +7,78 @@ class Factura (){
      var cantidadTotal: Int
 
     init {
+        //Inicializando datos con vacios
         this.carritoProductos= arrayListOf<Producto>()
         this.precioTotal=0.0
         this.cantidadTotal=0
     }
 
-    fun agregarProducto(producto: Producto){
+    fun agregarProducto(producto: Producto, cantidadProducto: Int){
         //Agregando producto a la lista del carrito
+        producto.setCantidadComprada(cantidadProducto)
         this.carritoProductos.add(producto)
         //Actualizando precioTotal y cantidad total de la venta
-        this.precioTotal+=producto.precioTotal
-        this.cantidadTotal+=producto.cantidadProducto
+        this.precioTotal+=producto.getPrecioProducto() * cantidadProducto
+        this.cantidadTotal+=cantidadProducto
     }
 
-    fun eliminarProducto(posicion: Int){
+    fun eliminarProducto(posicion: Int): String{
+
+        //Guardando producto actual en variable para facilitar un poco el manejo del dato
+        var productoActual = this.carritoProductos.get(posicion)
         //Restando del carrito el precio total del producto a eliminar
-        this.precioTotal -=this.carritoProductos.get(posicion).precioTotal
-        this.cantidadTotal -=this.carritoProductos.get(posicion).cantidadProducto
+        this.precioTotal -=(productoActual.getPrecioProducto()*productoActual.getCantidadComprada())
+        //Actualizando la cantidadTotal de productos que se mostraran en la factura
+        this.cantidadTotal -=productoActual.getCantidadComprada()
+        //Capturando el nombre del producto eliminado para operaciones futuras
+        var nombreProducto = productoActual.getNombreProducto()
         //Eliminando producto de la lista del carrito
         this.carritoProductos.removeAt(posicion);
+
+        //Retornando el nombre del producto eliminado
+        return nombreProducto
     }
 
+    //Funcion utilizada para conocer cuanto producto fue comprado, antes de eliminarlo para reestablecer la lista de productos iniciales
+    fun verificarCantidadCompradaDeProducto(posicion: Int):Int{
+        for (iterador in this.carritoProductos){
+            if(iterador.getNombreProducto()==this.carritoProductos.get(posicion).getNombreProducto()){
+               return iterador.getCantidadComprada()
+            }
+        }
+        return 0
+    }
+
+    //Mostrando el contenido de la factura a generar...
     fun mostrarFactura(){
         var i=1
         println("\n\n---------------------Lista de productos en carrito---------------------")
-        println("|| N ||Producto\t||Precio Unitario  ||Cantidad||Total del producto||")
+        println("|| N ||Producto\t||Precio U.|| Cantidad || Total del producto||")
         for (producto in this.carritoProductos){
-            producto.mostrarProducto(i)
+            producto.mostrarProductoFactura(i)
             i++
         }
         println("-----------------------------------------------------------------------")
         println("Cantidad de productos: "+this.cantidadTotal)
         println("Venta total: "+this.precioTotal)
-        println("-----------------------------------------------------------------------\n")
+        println("-----------------------------------------------------------------------")
 
+    }
+
+    //Mostrando el contenido de la factura generada y reinicializando algunos datos para la proxima factura
+    fun cerrarFactura(){
+        var i=1
+        println("\n\n---------------------Lista de productos en carrito---------------------")
+        println("|| N ||Producto\t||Precio U.|| Cantidad || Total del producto||")
+        for (producto in this.carritoProductos){
+            producto.mostrarProductoFactura(i)
+            producto.setCantidadComprada(-producto.getCantidadComprada())
+            i++
+        }
+        println("-----------------------------------------------------------------------")
+        println("Cantidad de productos: "+this.cantidadTotal)
+        println("Venta total: "+this.precioTotal)
+        println("-----------------------------------------------------------------------")
     }
 
 
--- src/main/kotlin/Producto.kt
+++ src/main/kotlin/Producto.kt
@@ -1,22 +1,52 @@
 class Producto(nombre: String, precio: Double, cantidad: Int){
-    //Propiedades de la clase Persona
+    //Propiedades de la clase Producto
     private var nombreProducto: String
     private var precioProducto: Double
-    var cantidadProducto: Int
-    var precioTotal: Double
+    var cantidadDisponible: Int
+    private var cantidadComprada: Int=0
+
 
     //Inicializador init para asignar los valores iniciales
     init{
         this.nombreProducto=nombre
         this.precioProducto=precio
-        this.cantidadProducto=cantidad
-        this.precioTotal=(precio*cantidad)
+        this.cantidadDisponible=cantidad
+        this.cantidadComprada=0
     }
 
     //Metodos de la clase Producto
+
+    //Utilizado al mostrar productosDisponibles al usuario
     fun mostrarProducto(i:Int){
 
-        println("|| "+i+" ||"+this.nombreProducto+"  ||"+this.precioProducto+"\t\t\t\t||"+this.cantidadProducto+"\t\t ||"+this.precioTotal)
+        println("|| "+i+" ||"+this.nombreProducto+"  || "+this.precioProducto+" || "+this.cantidadDisponible)
+    }
+
+    //Utilizado al mostrar productos en factura y por ende los que se encuentran en carrito de compra
+    fun mostrarProductoFactura(i:Int){
+        println("|| "+i+" ||"+this.nombreProducto+"  || "+this.precioProducto+" || "+this.cantidadComprada+" || "+(this.cantidadComprada*this.precioProducto))
+    }
+
+
+    //Getters y Setters
+    fun getNombreProducto(): String{
+        return this.nombreProducto
+    }
+
+    fun getPrecioProducto(): Double{
+        return this.precioProducto;
+    }
+
+    fun getCantidadComprada():Int{
+        return this.cantidadComprada
+    }
+
+    fun setCantidadComprada(cant: Int){
+        this.cantidadComprada= (this.cantidadComprada+cant)
+    }
+
+    fun reducirCantidadDisponible(cantComprada: Int){
+        this.cantidadDisponible= (this.cantidadDisponible-cantComprada)
     }
 
 }
